4420f10a00d8d0c0efbc11b29b929873bc24f66a
Davi Roque <sowdavi@gmail.com>
2023-12-15 02:39:47 -0300

    fix: refatory routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:project_02_diamond/components/drawer_task.dart';
-import 'package:project_02_diamond/screens/home_screen.dart';
-import 'package:project_02_diamond/screens/pergunta_01.dart';
-import 'package:project_02_diamond/screens/pergunta_02.dart';
-import 'package:project_02_diamond/screens/pergunta_03.dart';
-import 'package:project_02_diamond/screens/pergunta_04.dart';
-import 'package:project_02_diamond/screens/pergunta_05.dart';
-import 'package:project_02_diamond/screens/pergunta_06.dart';
-import 'package:project_02_diamond/screens/pergunta_07.dart';
-import 'package:project_02_diamond/screens/pergunta_08.dart';
-import 'package:project_02_diamond/screens/pergunta_09.dart';
-import 'package:project_02_diamond/screens/pergunta_10.dart';
-import 'package:project_02_diamond/screens/pergunta_11.dart';
-import 'package:project_02_diamond/screens/pergunta_12.dart';
+import 'package:project_02_diamond/routes/routes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,26 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      routes: {
-        '/': (context) => const TabBarDemo(child: TestMap()),
-        '/pergunta01': (context) => const TabBarDemo(child: Pergunta01()),
-        '/pergunta02': (context) => const TabBarDemo(child: Pergunta02()),
-        '/pergunta03': (context) => const TabBarDemo(child: Pergunta03()),
-        '/pergunta04': (context) => const TabBarDemo(child: Pergunta04()),
-        '/pergunta05': (context) => const TabBarDemo(child: Pergunta05()),
-        '/pergunta06': (context) => const TabBarDemo(child: Pergunta06()),
-        '/pergunta07': (context) => const TabBarDemo(child: Pergunta07()),
-        '/pergunta08': (context) => const TabBarDemo(child: Pergunta08()),
-        '/pergunta09': (context) => const TabBarDemo(child: Pergunta09()),
-        '/pergunta10': (context) => const TabBarDemo(child: Pergunta10()),
-        '/pergunta11': (context) => const TabBarDemo(child: Pergunta11()),
-        '/pergunta12': (context) => const TabBarDemo(child: Pergunta12()),
-      },
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        routes: getAppRoutes());
   }
 }
